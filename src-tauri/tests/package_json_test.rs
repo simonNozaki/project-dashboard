4f1c7b979a55b7d02f9688bee5b6f8e1d6fdb453
simonNozaki <tldk1993@gmail.com>
2024-01-10 21:11:35 +0900

    feat: コマンドカードの連打防止 (#4)
    * feat: コマンドカードの連打防止
    
    * chore: プロジェクトをクリアしたときもアラートを出す
    
    * docs: TODO更新
    
    * chore: スペースの位置を微調整
    
    * test: ユニットテストとCIの整備
    
    * test: cargo fmt

---
+++ src-tauri/tests/package_json_test.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod package_json_test {
+    use package_dashboard::foundations::package_json::to_project_meta;
+
+    #[test]
+    fn can_parse_empty_project() {
+        let raw = "{}".to_string();
+        let (name, scripts) = to_project_meta(raw);
+
+        assert_eq!(name, "無題のプロジェクト");
+        assert_eq!(scripts.is_empty(), true);
+    }
+
+    #[test]
+    fn can_parse_project() {
+        let raw = "{ \"name\": \"test\", \"scripts\": { \"build\": \"yarn build\" } }".to_string();
+        let (name, scripts) = to_project_meta(raw);
+
+        assert_eq!(name, "test");
+        assert_eq!(scripts.len(), 1);
+        assert_eq!(scripts.get("build"), Some(&"yarn build".to_string()));
+    }
+}
--- src/components/domains/ScriptsList.vue
+++ src/components/domains/ScriptsList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { invoke } from '@tauri-apps/api'
 import { listen } from '@tauri-apps/api/event'
 
@@ -15,11 +16,15 @@ const props = defineProps<Props>()
 
 const emit = defineEmits<Emit>()
 
+const isExecuting = ref(false)
+
 // コマンド:終了ステータスのハッシュマップ
 // コマンド実行のたびにコマンドと終了ステータスを更新する
 const exitStatuses = ref<Record<string, number>>({})
 
 async function executeScript(command: string) {
+  isExecuting.value = true
+
   // コマンド実行前に標準出力をクリアしておく
   emit('execute', '')
   await listen<{ text: string }>('on-print-stdout', (event) => {
@@ -32,6 +37,8 @@ async function executeScript(command: string) {
   } catch (e) {
     exitStatuses.value[command] = 1
   }
+
+  isExecuting.value = false
 }
 
 function toIcon(code: number): { name: string, color: string } {
@@ -43,23 +50,33 @@ function toIcon(code: number): { name: string, color: string } {
     color: '#EF4444'
   }
 }
+
+const classes = computed(() => ({
+  'card__button--disabled': isExecuting.value
+}))
 </script>
 
 <template>
   <ul>
     <template v-for="(script, command) in scripts" :key="command">
       <li>
-        <div class="card" @click="executeScript(command)">
-          <p class="card__title">{{ command }}</p>
-          <p class="card__script">{{ script }}</p>
-          <template v-if="exitStatuses[command] != undefined">
-            <div class="card__status">
-              <Icon
-                :name="toIcon(exitStatuses[command]).name"
-                :color="toIcon(exitStatuses[command]).color"
-                />
-            </div>
-          </template>
+        <div class="card">
+          <button
+            class="card__button"
+            @click="executeScript(command)"
+            :disabled="isExecuting"
+            :class="classes">
+            <p class="card__title">{{ command }}</p>
+            <p class="card__script">{{ script }}</p>
+            <template v-if="exitStatuses[command] != undefined">
+              <div class="card__status">
+                <Icon
+                  :name="toIcon(exitStatuses[command]).name"
+                  :color="toIcon(exitStatuses[command]).color"
+                  />
+              </div>
+            </template>
+          </button>
         </div>
       </li>
     </template>
@@ -68,15 +85,23 @@ function toIcon(code: number): { name: string, color: string } {
 
 <style>
 .card {
-  @apply bg-[#eeeeee] px-3 py-2 rounded mb-2 hover:bg-[#dddddd] hover:cursor-pointer;
+  @apply bg-[#eeeeee] rounded mb-2 hover:bg-[#dddddd] hover:cursor-pointer;
+}
+
+.card__button {
+  @apply w-full px-3 py-2;
+}
+
+.card__button--disabled {
+  @apply bg-[#dddddd] text-[#666666];
 }
 
 .card__title {
-  @apply font-semibold;
+  @apply font-semibold text-left;
 }
 
 .card__script {
-  @apply ml-5;
+  @apply ml-5 text-left;
 }
 
 .card__status {
